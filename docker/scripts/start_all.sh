5cd68d5c72ccb4c5a7df4c277179d6a7e1dbbfaa
Alessandro Giorgio Togna <107464960+agtogna@users.noreply.github.com>
2025-02-12 15:02:27 +0100

    Update start_all.sh
    Signed-off-by: Alessandro Giorgio Togna <107464960+agtogna@users.noreply.github.com>

--- docker/scripts/start_all.sh
+++ docker/scripts/start_all.sh
@@ -24,6 +24,7 @@ else
     echo `kubectl get secrets/algon-api-token -o json  | jq .data | jq -r '."algod.token"' | base64 -d` >  node/data/algod.token
     echo `kubectl get secrets/algon-api-token -o json  | jq .data | jq -r '."algod.admin.token"' | base64 -d` >  node/data/algod.admin.token
 fi
+export ALGON_TOKEN="$(cat node/data/algod.token)"
 export token=
 
 echo "INFO: Algod Node current status"
@@ -31,5 +32,8 @@ echo "INFO: Algod Node current status"
 echo "INFO: Fast catchup only if more than 10000 rounds are missing."
 /algorand/node/goal node catchup --force -m 10000 &
 
-# Do not kill the pod
-sleep infinity
+# Do not kill the pod and print a JSON status line every minute
+while true; do
+    curl --location "http://localhost:8080/v2/status?format=json" --header 'Content-Type: application/json' --header 'Accept: application/json' --header "X-Algo-API-Token: ${ALGON_TOKEN}"
+    sleep 60
+done
